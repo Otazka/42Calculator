ba293d0b5fcd3b1d31506b18e8fd7dd9f40f27c0
Ellie <102157178+Otazka@users.noreply.github.com>
2025-04-16 07:50:35 +0200

    java file

---
+++ 42Calculator.java
@@ -0,0 +1,10 @@
+
+class Calculator {
+    public static void main(String[] args) {
+        
+        for(int i = 0; i < args.length; i++) {
+            System.out.println("Argument " + i + ":" + args[i]);
+
+        }
+    }
+}
